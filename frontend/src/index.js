27a0d8fec041dd053f0d2a6ebb81b1fb1659b3a7
thankdt-bnk <thanhdt@bnksolution.com>
2018-11-13 10:49:42 +0700

    test dokcer

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const App = () =>(
+    <div>
+        Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
