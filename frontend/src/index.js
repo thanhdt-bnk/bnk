439391743e45597d1505f18e1be8a028b31197dc
thankdt-bnk <thanhdt@bnksolution.com>
2018-11-15 13:57:58 +0700

    This is for run test on aws

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import CheckBoxComponent from './CheckBoxComponent';
 
 const App = () =>(
     <div>
         Hello world New Change!!!!!!!!!!!!!!!!!!
-        <span style={{color:'green'}}>App</span>
+        <CheckBoxComponent labelOn="On" labelOff="Off"/>
     </div>
 )
 
